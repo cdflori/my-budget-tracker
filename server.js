80587bf5d36af9341a0ea55bafde71b4eee7c482
Christina Flores <cdflori1@gmail.com>
2020-12-22 18:20:23 -0700

    adden env to PORT on server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -20,8 +20,6 @@ mongoose.connect(
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
   }
 );
 
